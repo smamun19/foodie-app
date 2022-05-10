65b0982efd9bd11165a3876f27053a088c90de88
S.M.Abdullah Al Mamun <smamun19@gmail.com>
2022-05-10 17:29:54 +0600

    Test nested react navigation typing.

--- src/navigators/drawer.tsx
+++ src/navigators/drawer.tsx
@@ -2,33 +2,25 @@ import React from 'react';
 import {
   createDrawerNavigator,
   DrawerNavigationProp,
+  DrawerScreenProps,
 } from '@react-navigation/drawer';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import type {CompositeScreenProps} from '@react-navigation/native';
 import Initial from '../screens/initial';
 import Home from '../screens/home';
 import CustomDrawer from '../components/CustomDrawer';
+import {RootStackParamList} from './root-stack';
 
 export type DrawerParamList = {
-  Initial: undefined;
   Home: undefined;
-  Home2: undefined;
-  Home3: undefined;
-  Home4: undefined;
-  Home5: undefined;
-  Home6: undefined;
-  Home7: undefined;
-  Home8: undefined;
-  Home9: undefined;
-  Home0: undefined;
-  Home00: undefined;
-  Home01: undefined;
-  Home02: undefined;
+  Initial: undefined;
 };
 
 export type DrawerScreens = keyof DrawerParamList;
 
-export type DrawerScreensProps<T extends DrawerScreens> = DrawerNavigationProp<
-  DrawerParamList,
-  T
+export type DrawerScreensProps<T extends DrawerScreens> = CompositeScreenProps<
+  DrawerScreenProps<DrawerParamList, T>,
+  NativeStackScreenProps<RootStackParamList>
 >;
 
 const {Navigator, Screen} = createDrawerNavigator<DrawerParamList>();
@@ -41,18 +33,6 @@ const Drawer = () => {
       screenOptions={{headerShown: false}}>
       <Screen name="Home" component={Home} />
       <Screen name="Initial" component={Initial} />
-      <Screen name="Home0" component={Initial} />
-      <Screen name="Home00" component={Initial} />
-      <Screen name="Home01" component={Initial} />
-      <Screen name="Home02" component={Initial} />
-      <Screen name="Home2" component={Initial} />
-      <Screen name="Home3" component={Initial} />
-      <Screen name="Home4" component={Initial} />
-      <Screen name="Home5" component={Initial} />
-      <Screen name="Home6" component={Initial} />
-      <Screen name="Home7" component={Initial} />
-      <Screen name="Home8" component={Initial} />
-      <Screen name="Home9" component={Initial} />
     </Navigator>
   );
 };
