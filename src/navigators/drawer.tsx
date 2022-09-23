c13dc3d936f77da5eeaea6a5a6210a803406979c
S.M.Abdullah Al Mamun <smamun19@gmail.com>
2022-09-23 19:34:27 +0600

    Add drawer item icon.

--- src/navigators/drawer.tsx
+++ src/navigators/drawer.tsx
@@ -12,7 +12,9 @@ import Help from '../screens/help';
 import Orders from '../screens/orders';
 import Profile from '../screens/profile';
 import CustomDrawer from '../components/CustomDrawer';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import {RootStackParamList} from './root-stack';
+import {StyleSheet} from 'react-native';
 
 export type DrawerParamList = {
   Home: undefined;
@@ -37,15 +39,70 @@ const Drawer = () => {
     <Navigator
       initialRouteName="Home"
       drawerContent={props => <CustomDrawer {...props} />}
-      screenOptions={{headerShown: false}}>
-      <Screen name="Home" component={Home} />
-      <Screen name="Favourites" component={Favourites} />
-      <Screen name="Profile" component={Profile} />
-      <Screen name="Addresses" component={Addresses} />
-      <Screen name="Orders" component={Orders} />
-      <Screen name="Help" component={Help} />
+      screenOptions={{
+        headerShown: false,
+        drawerLabelStyle: styles.drawerLabelStyle,
+      }}>
+      <Screen
+        options={{
+          drawerIcon: ({color, size}) => (
+            <MaterialIcons name="home" size={size} color={color} />
+          ),
+        }}
+        name="Home"
+        component={Home}
+      />
+      <Screen
+        options={{
+          drawerIcon: ({color, size}) => (
+            <MaterialIcons name="favorite" size={size} color={color} />
+          ),
+        }}
+        name="Favourites"
+        component={Favourites}
+      />
+      <Screen
+        options={{
+          drawerIcon: ({color, size}) => (
+            <MaterialIcons name="perm-identity" size={size} color={color} />
+          ),
+        }}
+        name="Profile"
+        component={Profile}
+      />
+      <Screen
+        options={{
+          drawerIcon: ({color, size}) => (
+            <MaterialIcons name="location-on" size={size} color={color} />
+          ),
+        }}
+        name="Addresses"
+        component={Addresses}
+      />
+      <Screen
+        options={{
+          drawerIcon: ({color, size}) => (
+            <MaterialIcons name="list-alt" size={size} color={color} />
+          ),
+        }}
+        name="Orders"
+        component={Orders}
+      />
+      <Screen
+        options={{
+          drawerIcon: ({color, size}) => (
+            <MaterialIcons name="help-center" size={size} color={color} />
+          ),
+        }}
+        name="Help"
+        component={Help}
+      />
     </Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  drawerLabelStyle: {marginLeft: -20},
+});
 
 export default Drawer;
