d9e4b3365249429b1f0e11a57bf7f9193d967ae2
S.M.Abdullah Al Mamun <smamun19@gmail.com>
2022-09-30 21:06:30 +0600

    Add favourites screen.

--- src/screens/favourites.tsx
+++ src/screens/favourites.tsx
@@ -1,32 +1,42 @@
 import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import {StyleSheet} from 'react-native';
 import Container from '../components/Container';
+import CustomCard from '../components/CustomCard';
 import CustomHeader from '../components/CustomHeader';
 import {DrawerScreensProps} from '../navigators/drawer';
 
 const Favourites = ({navigation}: DrawerScreensProps<'Favourites'>) => {
   return (
     <Container
+      containerStyle={styles.containerStyle}
       header={
         <CustomHeader
           title="Favourites"
           onLeftPress={() => navigation.navigate('Home')}
         />
       }>
-      <Text>Favourites Page</Text>
+      <CustomCard
+        cardStyle={styles.card}
+        iconName="favorite"
+        imgBorderRadius={10}
+        imgStyle={styles.img}
+        title="Name"
+      />
+      <CustomCard
+        imgBorderRadius={10}
+        iconName="favorite"
+        cardStyle={styles.card}
+        imgStyle={styles.img}
+        title="Name"
+      />
     </Container>
   );
 };
 
 const styles = StyleSheet.create({
-  view: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  scrollview: {
-    flex: 1,
-  },
+  containerStyle: {margin: 10},
+  card: {marginBottom: 15},
+  img: {width: '100%', borderRadius: 20},
 });
 
 export default Favourites;
